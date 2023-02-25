d443a4ecb5d1a5d01270d2a248bc91bb8dc0b732
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-02-25 12:02:57 +0100

    API keys added to .env

--- services/pipelines/src/config/__init__.py
+++ services/pipelines/src/config/__init__.py
@@ -5,6 +5,7 @@ https://rednafi.github.io/digressions/python/2020/06/03/python-configs.html
 
 from pydantic import BaseModel
 
+from .api_keys import ApiKeys
 from .app import AppConfig
 from .azure import AzureConfig
 
@@ -14,6 +15,7 @@ class GlobalConfig(BaseModel):
     Global configurations.
     """
 
+    api_keys = ApiKeys()
     app = AppConfig()
     azure = AzureConfig()
 
