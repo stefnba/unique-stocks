703724a5673c6d09677f6e4206be77b0156c9ebb
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-02-26 14:47:00 +0100

    Mapping table added to migrations

--- services/database/db/utils/seed.py
+++ services/database/db/utils/seed.py
@@ -1,3 +1,4 @@
+# pylint: disable=E1101
 import csv
 
 from alembic import op
@@ -11,12 +12,12 @@ def seed_table_from_csv(file_path: str, table_name: str, columns: list[ColumnCla
     allow COPY command from a non-superuser.
 
     Args:
-        file_path (str): Path the .csv file
+        file_path (str): path the .csv file
         table_name (str): name of table to be populated
         *columns (ColumnClause): ColumnClause as provided by sqlalchemy
     """
     db_rows = []
-    with open(file_path, "r") as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         csv_reader = csv.DictReader(file)
 
         for file_rows in csv_reader:
