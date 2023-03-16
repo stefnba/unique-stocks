8a8d8ac25eaf2de873fd607f25a7021858047d71
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-03-16 15:05:48 +0100

    azure config file_system changed to datalake

--- services/pipelines/pipelines/services/config/azure.py
+++ services/pipelines/pipelines/services/config/azure.py
@@ -5,4 +5,4 @@ from services.config.env import EnvBaseConfig
 
 class AzureConfig(EnvBaseConfig):
     storage_account_url: str = Field(env="AZURE_STORAGE_ACCOUNT_URL", default=None)
-    file_system = "dev"
+    file_system = "datalake"
