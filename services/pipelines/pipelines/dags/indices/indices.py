783a4e5aa5aff7de929403c11473047d9108353b
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-03-17 16:19:47 +0100

    index and index members added

---
+++ services/pipelines/pipelines/dags/indices/indices.py
@@ -0,0 +1,44 @@
+# pylint: disable=W0106:expression-not-assigned, C0415:import-outside-toplevel
+# pylint: disable=W0106:expression-not-assigned, C0415:import-outside-toplevel
+# pyright: reportUnusedExpression=false
+from datetime import datetime
+
+from airflow import DAG
+from airflow.decorators import task
+from airflow.models import TaskInstance
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
+with DAG(
+    dag_id="indices",
+    schedule=None,
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    tags=["indices"],
+) as dag:
+
+    @task()
+    def ingest():
+        from services.jobs.indices.eod import EodIndexJobs
+
+        return EodIndexJobs.download_index_list()
+
+    @task
+    def process(**context: TaskInstance):
+        from services.jobs.indices.eod import EodIndexJobs
+
+        file_path = context["ti"].xcom_pull()
+        print(file_path)
+        return EodIndexJobs.process_index_list(file_path)
+
+    @task
+    def curate(**context: TaskInstance):
+        file_path = context["ti"].xcom_pull()
+        return "Indeices downloaded"
+
+    trigger_dag_index_members = TriggerDagRunOperator(
+        task_id="trigger_index_members",
+        trigger_dag_id="index_members",
+        wait_for_completion=False,
+    )
+
+    ingest() >> process() >> trigger_dag_index_members >> curate()
