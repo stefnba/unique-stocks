002516ccd09a330bc0dc1bfb2daf4dc44d68bdc1
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-03-18 16:26:32 +0100

    migration to ruff for linting

--- services/pipelines/pipelines/main.py
+++ services/pipelines/pipelines/main.py
@@ -1,21 +1,5 @@
-import services.jobs.exchanges.market_stack as market_stack
-from services.jobs.exchanges.eod import (
-    download_details_for_exchanges,
-    download_exchanges,
-    transform_exchanges,
-)
-from services.jobs.exchanges.iso import donwload_iso_exchange_list
-
-
 def main():
-    donwload_iso_exchange_list()
-    market_stack.download_exchanges()
-    download_exchanges()
-    download_details_for_exchanges()
-    transform_exchanges(
-        "raw/exchanges/EodHistoricalData/2023/02/25/\
-            20230225-114616_EodHistoricalData.json"
-    )
+    print(1)
 
 
 if __name__ == "__main__":
