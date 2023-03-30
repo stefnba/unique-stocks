9e58ef4a45d7a2271872decd1a3bfe306ceb87fc
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-03-30 20:38:07 +0200

    added field to filters

--- services/pipelines/pipelines/shared/clients/db/postgres/repositories/mappings.py
+++ services/pipelines/pipelines/shared/clients/db/postgres/repositories/mappings.py
@@ -5,8 +5,12 @@ from shared.utils.sql.file import QueryFile
 
 
 class MappingsRepository(PgRepositories):
-    def get_mappings(self, source: Optional[str] = None, product: Optional[str] = None):
+    def get_mappings(self, source: Optional[str] = None, product: Optional[str] = None, field: Optional[str] = None):
         return self._query.find(
             QueryFile({"base_path": __file__, "path": "./sql/get_mappings.sql"}),
-            filters=[{"column": "source", "value": source}, {"column": "product", "value": product}],
+            filters=[
+                {"column": "source", "value": source},
+                {"column": "product", "value": product},
+                {"column": "field", "value": field},
+            ],
         ).get_polars_df()
