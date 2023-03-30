7c39b542bae4c80aa702f4609693fe348003e6c1
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-03-30 10:47:29 +0200

    moved mappings query to sql file

--- services/pipelines/pipelines/shared/clients/db/postgres/repositories/mappings.py
+++ services/pipelines/pipelines/shared/clients/db/postgres/repositories/mappings.py
@@ -1,11 +1,12 @@
 from typing import Optional
 
 from shared.clients.db.postgres.repositories.repository import PgRepositories
+from shared.utils.sql.file import QueryFile
 
 
 class MappingsRepository(PgRepositories):
     def get_mappings(self, source: Optional[str] = None, product: Optional[str] = None):
         return self._query.find(
-            "SELECT * FROM mappings",
+            QueryFile({"base_path": __file__, "path": "./sql/get_mappings.sql"}),
             filters=[{"column": "source", "value": source}, {"column": "product", "value": product}],
         ).get_polars_df()
