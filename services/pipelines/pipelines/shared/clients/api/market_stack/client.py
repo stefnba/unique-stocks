260214c18090f6b22073b96559258aa15b687265
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-04-01 14:11:36 +0200

    removed mic_correction and virtual_exchanges

--- services/pipelines/pipelines/shared/clients/api/market_stack/client.py
+++ services/pipelines/pipelines/shared/clients/api/market_stack/client.py
@@ -10,11 +10,6 @@ class MarketStackApiClient(ApiHook):
     client_key = "MarketStack"
     client_key_short = "msk"
 
-    mic_correction = {
-        "XETRA": "XETR",
-    }
-    virtual_exchanges = ["INDX"]
-
     _base_url = "http://api.marketstack.com/v1"
     _base_params = {"access_key": config.api_keys.market_stack, "limit": 1000}
 
