d9594f667434fc9479a93a7bf44a980e0d159494
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-05-16 21:49:53 +0200

    added data products

--- services/pipelines/pipelines/shared/config/app.py
+++ services/pipelines/pipelines/shared/config/app.py
@@ -1,6 +1,13 @@
-from pydantic import Field
+from typing import Literal
+
+from pydantic import BaseSettings, Field
 from shared.config.env import EnvBaseConfig
 from shared.config.types import EnvironmentTypes
+
+
+class DataConfig(BaseSettings):
+    products = Literal["exchange", "security", "security_ticker", "security_listing", "security_quote"]
+    sources = Literal["EodHistoricalData"]
 
 
 class AppConfig(EnvBaseConfig):
