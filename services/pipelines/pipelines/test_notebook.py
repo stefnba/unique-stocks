6cd9fc2ed43cbf2feb497a60f33ff3028f16fa7c
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-05-21 15:50:08 +0200

    test notebooks updated

--- services/pipelines/pipelines/test_notebook.py
+++ services/pipelines/pipelines/test_notebook.py
@@ -1,89 +1,35 @@
 # %%
-import io
-import json
-
-import polars as pl
-from dags.exchanges.exchange_details.jobs.exchange_details import ExchangeDetailsJobs
-from dags.exchanges.exchange_securities.jobs.eod import EodExchangeSecurityJobs
-from dags.exchanges.exchanges.jobs.eod import EodExchangeJobs
-from dags.exchanges.exchanges.jobs.iso import IsoExchangeJobs
-from dags.exchanges.exchanges.jobs.msk import MarketStackExchangeJobs
-from dags.indices.index_members.jobs.index_members import IndexMembersJobs
-from dags.quotes.historical.jobs.historical import HistoricalQuotesJobs
-from shared.clients.api.eod.client import EodHistoricalDataApiClient
-from shared.clients.api.open_figi.client import OpenFigiApiClient
-from shared.clients.datalake.azure.azure_datalake import datalake_client
-from shared.clients.db.postgres.repositories import DbQueryRepositories
-from shared.clients.duck.client import duck
-
-# %%
+exchange_code = "OTCQX"
 
 
-# get data from datalake with duck.get_data()
-quotes = duck.get_data(
-    "processed/product=quotes/security=AAPL/exchange=US/asset_source=EodHistoricalData/year=2023/month=04/20230401_processed_EodHistoricalData_AAPL_US.parquet",
-    handler="azure_abfs",
-    format="parquet",
-).df()
+def ingest(exchange: str):
+    from dags.security.eod_historical_data.jobs import ASSET_SOURCE, ingest
+    from dags.security.path import SecurityPath
+    from shared.hooks.data_lake import data_lake_hooks
 
-securities = duck.get_data(
-    "processed/product=exchanges/asset=exchange_securities/exchange=US/source=EodHistoricalData/year=2023/month=04/20230401_EodHistoricalData_US_exchange_securities_processed.parquet",
-    handler="azure_abfs",
-).df()
+    return data_lake_hooks.checkout(
+        func=lambda: ingest(exchange),
+        commit_path=SecurityPath.raw(source=ASSET_SOURCE, bin=exchange_code, file_type="json"),
+    )
 
-# execute query on duckDB with duck.query()
-duck.query(
-    "SELECT * FROM $quotes quotes LEFT JOIN $securities securities ON quotes.security_source_code = securities.source_code",
-    # supply variables, needs to be $variable in query statement
-    quotes=quotes,
-    securities=securities,
-).df()
 
-# %%
-# get list of parquet files from datalake, currenly not possible with .get_data()
-files = ["", "", ""]
-# build abfs path from pure datalake path with duck.helpers.build_abfs_path() for each path supplied
-duck.db.read_parquet([duck.helpers.build_abfs_path(file) for file in files])
+def transform(file_path: str):
+    from dags.security.eod_historical_data.jobs import ASSET_SOURCE, transform
+    from dags.security.path import SecurityPath
+    from shared.hooks.data_lake import data_lake_hooks
+
+    return data_lake_hooks.checkout(
+        checkout_path=file_path,
+        func=lambda data: transform(data),
+        commit_path=SecurityPath.processed(source=ASSET_SOURCE, bin=exchange_code),
+    )
 
 
 # %%
 
-path = EodExchangeJobs.download_exchange_list()
-df = duck.get_data(path, handler="azure_abfs", format="json").pl()
+path = ingest(exchange_code)
+path
 
 
 # %%
-
-df = duck.query("SELECT * FROM $test", test=df).df()
-
-df.head()
-
-
-# %%
-
-
-def get_quote_for_members_of_indices():
-    """
-    Uses same jobs that airflow dags use. Jobs usually return file_path from datalake that are then supplied are args to next job.
-    """
-    # indicies = ["GDAXI", "FTSE"]
-    indicies = ["DJI", "SSMI", "NDX"]
-    # indicies = ["GSPC"]
-
-    transformed_paths = []
-
-    for index in indicies:
-        raw_path = IndexMembersJobs.download(index)
-        transformed_path = IndexMembersJobs.transform(raw_path)
-
-        if transformed_path:
-            transformed_paths.append(duck.helpers.build_abfs_path(transformed_path))
-
-    data = duck.db.read_parquet(transformed_paths)
-    for member in data.df()[["code", "exchange_source_code"]].to_dict("records"):
-        raw_path = HistoricalQuotesJobs.download(member["code"], member["exchange_source_code"])
-
-        HistoricalQuotesJobs.transform(raw_path, member["code"], member["exchange_source_code"])
-
-
-get_quote_for_members_of_indices()
+transform(path)
