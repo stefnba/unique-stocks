ae57cd4eb399f12d5c8a930e20239f8106129af0
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-06-12 13:50:59 +0200

    added http handler

--- services/pipelines/pipelines/shared/utils/logging/handlers.py
+++ services/pipelines/pipelines/shared/utils/logging/handlers.py
@@ -1,8 +1,14 @@
 import logging
-from typing import Optional
+from typing import Optional, cast
+import requests
+import json
 
+# from requests.adapters import HTTPAdapter
+# from requests.packages.urllib3.util.retry import Retry
+from logging import LogRecord, handlers
 from shared.utils.logging.formatter import BaseFormatter, JSONFormatter
 from shared.utils.logging.types import FileHandlerFormats
+from shared.config import CONFIG
 
 ROOT_FORMAT = (
     "%(asctime)s-%(levelname)s-%(extra)s-%(name)s::[%(filename)s:%(lineno)d]::%(module)s|%(lineno)s:: %(message)s"
@@ -23,7 +29,70 @@ class BaseHandler:
         return logging.Handler()
 
 
+class CustomHttp(logging.Handler):
+    # host: str
+    # port: int
+    # endpoint: str
+    url: str
+    session: requests.Session
+
+    def __init__(
+        self,
+    ) -> None:
+        # self.host = CONFIG.logging.host
+        # self.endpoint = CONFIG.logging.endpoint
+        # self.port = CONFIG.logging.port
+
+        self.url = f"{CONFIG.logging.host}:{CONFIG.logging.port}/{ CONFIG.logging.endpoint.lstrip('/')}"
+        print(self.url)
+        # sets up a session with the server
+        # MAX_POOLSIZE = 100
+
+        self.session = requests.Session()
+        self.session.headers.update(
+            {
+                "Content-Type": "application/json",
+                # 'Authorization': 'Bearer %s' % (self.token)
+            }
+        )
+
+        super().__init__()
+
+    def emit(self, record: LogRecord) -> None:
+        # convert json string to dict
+        record_dict = json.loads(self.format(record))
+
+        extra = record_dict.pop("extra")
+
+        # add service property for remote service
+        record_dict["service"] = "pipeline"
+
+        # post log to remote service
+        self.session.post(self.url, data=json.dumps({**record_dict, **extra}))
+
+
+class CustomHttpHandler(handlers.HTTPHandler):
+    def __init__(self) -> None:
+        host = CONFIG.logging.host
+        endpoint = CONFIG.logging.endpoint
+        port = CONFIG.logging.port
+        method = "POST"
+
+        super().__init__(host=f"{host}:{port}", url=endpoint, method=method)
+
+    def mapLogRecord(self, record: logging.LogRecord):
+        return {"service": "Pipeline", "test": 123}
+
+
 class Handlers:
+    class Http(BaseHandler):
+        format = JSONFormatter(None)
+        # def __init__(self) -> None:
+        #     super().__init__(format="JSON")
+
+        def get_handler(self):
+            return CustomHttp()
+
     class Console(BaseHandler):
         """
         Logs to console.
