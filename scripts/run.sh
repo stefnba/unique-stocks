e974072ff8c77bf9ef737087b3b9c1ab1b3037a8
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-07-07 20:07:24 +0200

    updated run scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,7 @@
 
 cwd=$(pwd)
 
+# Start docker network
 network_name=unique-stocks-network
 docker network inspect $network_name >/dev/null 2>&1 || \
 		docker network create --driver bridge $network_name && echo "✅ Network $network_name created"
@@ -9,7 +10,7 @@ docker network inspect $network_name >/dev/null 2>&1 || \
 # Database service
 cd ./services/database
 echo "Spinning up database service ..."
-docker compose up -d
+make docker-up
 make migrate-up
 echo "✅ Database running and migrations completed"
 
@@ -19,3 +20,24 @@ cd ./services/pipelines
 echo "Spinning up pipelines service ..."
 make docker-up
 echo "✅ Pipelines service running"
+
+
+# Backend service
+cd $cwd
+cd ./services/backend
+npm run docker:dev
+echo "✅ Backend service running"
+
+
+# Log service
+cd $cwd
+cd ./services/log
+npm run docker:dev
+echo "✅ Log service running"
+
+
+# Frontend service
+cd $cwd
+cd ./services/frontend
+npm run docker:dev
+echo "✅ Frontend service running"
