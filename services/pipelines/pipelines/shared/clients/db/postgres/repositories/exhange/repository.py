173accef88f058fe702acff3f44d8762d55474e9
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-07-16 15:07:04 +0200

    fixed repo query

--- services/pipelines/pipelines/shared/clients/db/postgres/repositories/exhange/repository.py
+++ services/pipelines/pipelines/shared/clients/db/postgres/repositories/exhange/repository.py
@@ -1,22 +1,23 @@
 from shared.clients.db.postgres.repositories.base import PgRepositories
 from shared.clients.db.postgres.repositories.exhange.schema import Exchange
 from shared.utils.sql.file import QueryFile
+from typing import Optional
 
 
 class ExchangeRepository(PgRepositories):
     table = ("data", "exchange")
     schema = Exchange
 
-    def find_all(self):
+    def find_all(self, source: Optional[str] = None):
         return self._query.find(
-            "SELECT * FROM exchange",
-        ).get_all()
+            "SELECT * FROM data.exchange", filters=[{"column": "source", "operator": "EQUAL", "value": source}]
+        ).get_polars_df(schema=self.schema)
 
     def add(self, data):
         return self._query.add(
             data,
             table=self.table,
-            column_model=Exchange,
+            column_model=self.schema,
             returning="ALL_COLUMNS",
             conflict={
                 "target": ["id"],
