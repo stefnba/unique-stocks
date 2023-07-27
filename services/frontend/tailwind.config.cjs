ecc8bb64fa28373d6d5feda4bb795a0ea4c5d2b6
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-07-27 10:01:52 +0200

    added source to tailwind tutorial

--- services/frontend/tailwind.config.cjs
+++ services/frontend/tailwind.config.cjs
@@ -1,3 +1,4 @@
+// https://dev.to/ivadyhabimana/setup-tailwind-css-in-a-react-project-configured-from-scratch-a-step-by-step-guide-2jc8
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.{ts,tsx}', './public/index.html'],
