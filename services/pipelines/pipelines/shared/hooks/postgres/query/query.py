d9f90570f22cddc9dbd3fd8681df4901e5e530ce
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-08-18 16:07:00 +0200

    feat: stream query results

--- services/pipelines/pipelines/shared/hooks/postgres/query/query.py
+++ services/pipelines/pipelines/shared/hooks/postgres/query/query.py
@@ -3,7 +3,8 @@ from shared.hooks.postgres.query.find import FindQuery
 from shared.hooks.postgres.query.run import RunQuery
 from shared.hooks.postgres.query.update import UpdateQuery
 from shared.hooks.postgres.query.copy import CopyQuery
+from shared.hooks.postgres.query.stream import StreamQuery
 
 
-class PgQuery(AddQuery, FindQuery, RunQuery, UpdateQuery, CopyQuery):
+class PgQuery(AddQuery, FindQuery, RunQuery, UpdateQuery, CopyQuery, StreamQuery):
     pass
