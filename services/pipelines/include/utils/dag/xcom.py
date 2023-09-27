d2657f4a0d90cd8901ed7fd8801555edf0ab02f3
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-09-27 15:26:08 +0200

    feat: re-factoring of xcom

--- services/pipelines/include/utils/dag/xcom.py
+++ services/pipelines/include/utils/dag/xcom.py
@@ -8,15 +8,34 @@ from typing import TypeVar, Any, Optional, overload, Type
 R = TypeVar("R")
 
 
+class XComSetter:
+    @staticmethod
+    def set(key: str, value: Any):
+        """
+        Set a XComs value. Can only be used inside a `task` function.
+        """
+        context = get_current_context()
+
+        task_instance = context.get("ti")
+        if isinstance(task_instance, TaskInstance):
+            return task_instance.xcom_push(key=key, value=value)
+
+        raise AirflowException(
+            "Current context was requested but no context was found! Are you running within an airflow task?"
+        )
+
+
 class XComGetter:
-    """Helper that can be used outside of `task` functions to get xcom values."""
+    """Get XComs value outside of `task` functions to get xcom values."""
 
     task_id: str | list[str]
     key: str
+    use_map_index: bool
 
-    def __init__(self, task_id: str | list[str], key: str = "return_value") -> None:
+    def __init__(self, task_id: str | list[str], key: str = "return_value", use_map_index=True) -> None:
         self.task_id = task_id
         self.key = key
+        self.use_map_index = use_map_index
 
     def parse(self, context: Context):
         """Retrieve XComs key from Airflow context."""
@@ -31,55 +50,70 @@ class XComGetter:
             xcom_value = ti.xcom_pull(task_ids=task_id, key=key, map_indexes=index)
             return xcom_value
 
+    def pull(self):
+        """
+        Retrieve a XComs key. Can only be used inside a `task` function.
+        """
+        context = get_current_context()
 
-def get_xcom_template(task_id: str, key: str = "return_value", use_map_index=True):
-    """Helper to build a Jinja templating string to get a XComs value."""
+        task_instance = context.get("ti")
+        if isinstance(task_instance, TaskInstance):
+            index = -1
 
-    map_index = "task_instance.map_index" if use_map_index else -1
-    template = f"{{{{ task_instance.xcom_pull(task_ids='{task_id}', key='{key}', map_indexes={map_index}) }}}}"
-    return template
+            if self.use_map_index and isinstance(task_instance.map_index, int):
+                index = task_instance.map_index
 
+            value = task_instance.xcom_pull(task_ids=self.task_id, key=self.key, map_indexes=index)
 
-@overload
-def get_xcom_value(task_id: str, key: str = "return_value", *, return_type: Type[R]) -> R:
-    ...
+            return value
 
+        raise AirflowException(
+            "Current context was requested but no context was found! Are you running within an airflow task?"
+        )
 
-@overload
-def get_xcom_value(task_id: str, key: str = "return_value", return_type=None) -> None:
-    ...
+    @staticmethod
+    def pull_with_template(task_id: str, key: str = "return_value", use_map_index=True) -> str:
+        """Helper to build a Jinja templating string to get a XComs value."""
 
+        map_index = "task_instance.map_index" if use_map_index else -1
+        template = f"{{{{ task_instance.xcom_pull(task_ids='{task_id}', key='{key}', map_indexes={map_index}) }}}}"
+        return template
 
-def get_xcom_value(task_id: str, key: str = "return_value", return_type: Optional[Type[R]] = None) -> R | Any:
-    """
-    Retrieve a XComs key. Can only be used inside a `task` function.
-    """
-
-    context = get_current_context()
-
-    task_instance = context.get("ti")
-    if isinstance(task_instance, TaskInstance):
-        index = task_instance.map_index  # if no mapped task, map_index is -1
-
-        value = task_instance.xcom_pull(task_ids=task_id, key=key, map_indexes=index if isinstance(index, int) else -1)
-
-        return value
-
-    raise AirflowException(
-        "Current context was requested but no context was found! Are you running within an airflow task?"
-    )
+    @classmethod
+    def pull_now(cls, task_id: str, key: str = "return_value", use_map_index=True):
+        return cls(task_id=task_id, key=key, use_map_index=use_map_index).pull()
 
 
-def set_xcom_value(key: str, value: Any):
-    """
-    Set a XComs value. Can only be used inside a `task` function.
-    """
-    context = get_current_context()
+# @overload
+# def get_xcom_value(task_id: str, key: str = "return_value", *, use_map_indexes=True, return_type: Type[R]) -> R:
+#     ...
 
-    task_instance = context.get("ti")
-    if isinstance(task_instance, TaskInstance):
-        return task_instance.xcom_push(key=key, value=value)
 
-    raise AirflowException(
-        "Current context was requested but no context was found! Are you running within an airflow task?"
-    )
+# @overload
+# def get_xcom_value(task_id: str, key: str = "return_value", use_map_indexes=True, return_type=None) -> None:
+#     ...
+
+
+# def get_xcom_value(
+#     task_id: str, key: str = "return_value", use_map_indexes=True, return_type: Optional[Type[R]] = None
+# ) -> R | Any:
+#     """
+#     Retrieve a XComs key. Can only be used inside a `task` function.
+#     """
+
+#     context = get_current_context()
+
+#     task_instance = context.get("ti")
+#     if isinstance(task_instance, TaskInstance):
+#         index = -1
+
+#         if use_map_indexes and isinstance(task_instance.map_index, int):
+#             index = task_instance.map_index
+
+#         value = task_instance.xcom_pull(task_ids=task_id, key=key, map_indexes=index)
+
+#         return value
+
+#     raise AirflowException(
+#         "Current context was requested but no context was found! Are you running within an airflow task?"
+#     )
