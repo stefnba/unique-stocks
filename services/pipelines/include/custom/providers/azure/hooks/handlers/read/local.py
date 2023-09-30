32a161572a4d5f277a537aabb5b5dfba9f79d982
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-09-30 12:20:52 +0200

    feat: handlers added

--- services/pipelines/include/custom/providers/azure/hooks/handlers/read/local.py
+++ services/pipelines/include/custom/providers/azure/hooks/handlers/read/local.py
@@ -6,8 +6,14 @@ from custom.providers.azure.hooks.handlers.base import DatasetHandler
 class LocalDatasetArrowHandler(DatasetHandler):
     """Reads a dataset from local filesystem with `pyarrow.dataset`."""
 
-    def read(self, schema=None, **kwargs) -> ds.FileSystemDataset:
-        return ds.dataset(source=self.path.uri, format=self.format, schema=schema, **kwargs)
+    def read(self, schema=None, partitioning="hive", **kwargs) -> ds.FileSystemDataset:
+        return ds.dataset(
+            source=self.path.uri,
+            format=self.format,
+            schema=schema,
+            partitioning=partitioning,
+            **kwargs,
+        )
 
 
 class LocalDatasetReadHandler(DatasetHandler):
@@ -16,10 +22,17 @@ class LocalDatasetReadHandler(DatasetHandler):
     Works only for `parquet` and `csv` files.
     """
 
-    def read(self, **kwargs) -> pl.LazyFrame:
+    def read(self, hive_partitioning: bool = True, **kwargs) -> pl.LazyFrame:
         if self.format == "parquet":
-            return pl.scan_parquet(self.path.uri)
+            return pl.scan_parquet(
+                self.path.uri,
+                hive_partitioning=hive_partitioning,
+                **kwargs,
+            )
         if self.format == "csv":
-            return pl.scan_csv(self.path.uri)
+            return pl.scan_csv(
+                self.path.uri,
+                **kwargs,
+            )
 
         raise ValueError("File format not supported.")
--- services/pipelines/include/custom/providers/azure/hooks/handlers/write/local.py
+++ services/pipelines/include/custom/providers/azure/hooks/handlers/write/local.py
@@ -1,6 +1,8 @@
 import polars as pl
 from custom.providers.azure.hooks.handlers.base import DatasetHandler
 from custom.providers.azure.hooks.types import Dataset
+from pyarrow import dataset as ds
+import pyarrow as pa
 
 
 class LocalDatasetWriteHandler(DatasetHandler):
@@ -11,5 +13,43 @@ class LocalDatasetWriteHandler(DatasetHandler):
     def write(self, dataset: Dataset, **kwargs):
         if isinstance(dataset, pl.LazyFrame):
             dataset.sink_parquet(path=self.path.uri)
+
+        return self.path
+
+
+class LocalDatasetWriteArrowHandler(DatasetHandler):
+    """Write a dataset to local filesystem using `pyarrow.Dataset.write_dataset()`."""
+
+    def write(
+        self,
+        dataset: Dataset,
+        existing_data_behavior="error",
+        basename_template=None,
+        partitioning_flavor="hive",
+        format="parquet",
+        **kwargs,
+    ):
+        ds.write_dataset(
+            data=dataset,
+            base_dir=self.path.uri,
+            format=format or self.format,
+            existing_data_behavior=existing_data_behavior,
+            partitioning_flavor=partitioning_flavor,
+            basename_template=basename_template,
+            schema=pa.schema(
+                [
+                    pa.field("date", pa.string()),
+                    pa.field("open", pa.float64()),
+                    pa.field("high", pa.float64()),
+                    pa.field("low", pa.float64()),
+                    pa.field("close", pa.float64()),
+                    pa.field("adjusted_close", pa.float64()),
+                    pa.field("volume", pa.int64()),
+                    pa.field("exchange_code", pa.string()),
+                    pa.field("security_code", pa.string()),
+                ]
+            ),
+            **kwargs,
+        )
 
         return self.path
