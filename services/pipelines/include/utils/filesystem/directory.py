b576482551d29d05a0779ccd80ec8c9e66125376
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-10-12 14:19:39 +0200

    feat: added asyncio and various filesystem ops

---
+++ services/pipelines/include/utils/filesystem/directory.py
@@ -0,0 +1,35 @@
+import typing
+from dataclasses import dataclass
+import os
+from pathlib import Path
+
+
+@dataclass
+class DirFile:
+    path: str
+    filename: str
+    rel_path: str
+
+
+def scan_dir_files(dir: str, include_sub_dirs=True) -> typing.Generator[DirFile, None, None]:
+    """
+    Creates generator of files residing in a directory.
+
+    Args:
+        dir (str):
+            Path to directory.
+        include_sub_dirs (bool, optional):
+            Whether to also walk into sub directories or not.
+
+    Yields:
+        DirFile: Dataclass containiner path, filename and path relative to `dir` arg.
+    """
+    if include_sub_dirs:
+        for root, _, files in os.walk(dir):
+            for file in files:
+                path = os.path.join(root, file)
+                yield DirFile(path=path, filename=file, rel_path=str(Path(path).relative_to(dir)))
+    else:
+        for filename in os.scandir(dir):
+            if filename.is_file() and not filename.name.startswith("."):
+                yield DirFile(path=filename.path, filename=filename.name, rel_path=filename.name)
