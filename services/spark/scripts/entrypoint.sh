7fb92bc0c6770d87f83f217f5a5cbd23544f180c
Stefan Bauer <stefanjakob.bauer@gmail.com>
2024-01-16 16:39:00 +0100

    feat: ssh access

--- services/spark/scripts/entrypoint.sh
+++ services/spark/scripts/entrypoint.sh
@@ -6,9 +6,14 @@ echo "SPARK_WORKLOAD: $SPARK_WORKLOAD"
 
 if [ "$SPARK_WORKLOAD" == "master" ];
 then
-    /usr/sbin/sshd # start ssh server
-    echo "Starting Spark Master..."
+    # start ssh server
+    echo "Starting SSH..."
+    /usr/sbin/sshd 
+    # set environment variables
+    echo "Setting environment variables..."
     (env | sed 's/^/export /') > /root/environment_variables.sh
+    # start spark master
+    echo "Starting Spark Master..."
     start-master.sh -p $SPARK_MASTER_PORT
 elif [ "$SPARK_WORKLOAD" == "worker" ];
 then
