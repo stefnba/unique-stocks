f5c1557a9cb31ee64434d20131830f55d45f5770
Stefan Bauer <stefanjakob.bauer@gmail.com>
2024-01-17 17:36:25 +0100

    feat: col selection and row filter added

--- services/pipelines/include/custom/providers/iceberg/hooks/pyiceberg.py
+++ services/pipelines/include/custom/providers/iceberg/hooks/pyiceberg.py
@@ -1,11 +1,18 @@
 import typing as t
 
+import polars as pl
 from airflow.exceptions import AirflowException
 from airflow.hooks.base import BaseHook
+from pyiceberg import expressions
 from pyiceberg.catalog import Catalog, load_catalog
+from pyiceberg.expressions import AlwaysTrue, BooleanExpression
 from pyiceberg.table import Table
 
 CatalogType = t.Literal["glue"]
+
+ALWAYS_TRUE = AlwaysTrue()
+
+filter_expressions = expressions
 
 
 class IcebergHook(BaseHook):
@@ -91,16 +98,50 @@ class IcebergHook(BaseHook):
     def load_table(self, table_name: str) -> Table:
         return self.load_catalog(self.catalog_name).load_table(table_name)
 
-    def to_arrow(self):
-        return self.table.scan().to_arrow()
+    def to_arrow(
+        self,
+        row_filter: t.Union[str, BooleanExpression] = ALWAYS_TRUE,
+        selected_fields: t.Tuple[str, ...] = ("*",),
+    ):
+        return self.table.scan(
+            row_filter=row_filter,
+            selected_fields=selected_fields,
+        ).to_arrow()
 
-    def to_pandas(self):
-        return self.table.scan().to_pandas()
+    def to_pandas(
+        self,
+        row_filter: t.Union[str, BooleanExpression] = ALWAYS_TRUE,
+        selected_fields: t.Tuple[str, ...] = ("*",),
+    ):
+        return self.table.scan(
+            row_filter=row_filter,
+            selected_fields=selected_fields,
+        ).to_pandas()
 
-    def to_duckdb(self, table_name: str):
-        return self.table.scan().to_duckdb(table_name)
+    def to_duckdb(
+        self,
+        table_name: str,
+        row_filter: t.Union[str, BooleanExpression] = ALWAYS_TRUE,
+        selected_fields: t.Tuple[str, ...] = ("*",),
+    ):
+        return self.table.scan(
+            row_filter=row_filter,
+            selected_fields=selected_fields,
+        ).to_duckdb(table_name)
 
-    def to_polars(self):
-        import polars as pl
+    def to_polars(
+        self,
+        row_filter: t.Union[str, BooleanExpression] = ALWAYS_TRUE,
+        selected_fields: t.Tuple[str, ...] = ("*",),
+    ) -> pl.DataFrame:
+        data = pl.from_arrow(
+            self.to_arrow(
+                row_filter=row_filter,
+                selected_fields=selected_fields,
+            )
+        )
 
-        return pl.from_arrow(self.to_arrow())
+        if not isinstance(data, pl.DataFrame):
+            raise TypeError("Data is not a polars DataFrame.")
+
+        return data
