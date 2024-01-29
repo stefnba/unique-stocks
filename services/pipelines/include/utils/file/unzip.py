7f2f0721909cb3fa60fed9a96bd69423ecd00a0f
Stefan Bauer <stefanjakob.bauer@gmail.com>
2024-01-29 15:30:52 +0100

    feat: added compress_with_gzip func

--- services/pipelines/include/utils/file/unzip.py
+++ services/pipelines/include/utils/file/unzip.py
@@ -1,5 +1,8 @@
+import gzip
 import os
 import zipfile
+from pathlib import Path as P
+from uuid import uuid4
 
 from utils.filesystem.path import LocalPath, Path, PathInput
 
@@ -21,3 +24,35 @@ def unzip_file(path: PathInput, delete_zip_file=False) -> Path:
         os.remove(path.uri)
 
     return LocalPath.create(unzipped_file_path)
+
+
+def compress_with_gzip(path: str, chunk_size=8192, delete_source_file=True) -> str:
+    """
+    Compress a file on a local file system using gzip.
+    The compressed file is saved in the same directory as the source file.
+
+    Args:
+        path (str): Path to local uncompressed file.
+        chunk_size (int, optional): _description_. Defaults to 8,192.
+        delete_source_file (bool, optional): _description_. Defaults to True.
+
+    Returns:
+        str: Absolute path to local compressed file.
+    """
+
+    filename = uuid4().hex
+    ext = P(path).suffix.replace(".", "")
+
+    zip_path = P(path).parent / f"{filename}.{ext}.gz"
+
+    with open(path, "rb") as f_in, gzip.open(zip_path, "wb") as f_out:
+        while True:
+            content = f_in.read(chunk_size)
+            if not content:
+                break
+            f_out.write(content)
+
+    if delete_source_file:
+        os.remove(path)
+
+    return zip_path.absolute().as_posix()
